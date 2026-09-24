9dc4b19df9ed3e66a6c8ce882b04e3326ad76e8f
Sanya Gupta <46195995+gsanya1003@users.noreply.github.com>
2026-09-24 11:16:15 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,52 @@
+class TrieNode{
+    HashMap<Character, TrieNode> child ;
+    boolean  last;
+   
+       TrieNode(){
+        last = false;
+         child = new HashMap<>();
+       }
+       
+    
+}
+
+class PrefixTree {
+  
+   TrieNode root ;
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+         TrieNode curr = root;
+         for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            curr.child.putIfAbsent(c, new TrieNode());
+            curr = curr.child.get(c);
+         }
+         curr.last = true;
+    }
+
+    public boolean search(String word) {
+         TrieNode curr = root;
+         for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(!curr.child.containsKey(c))
+            return false;
+            curr = curr.child.get(c);
+         }
+           return curr.last;
+
+    }
+
+    public boolean startsWith(String word) {
+          TrieNode curr = root;
+         for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+              if(!curr.child.containsKey(c))
+             return false;
+             curr = curr.child.get(c);
+         }
+           return true;
+    }
+}
